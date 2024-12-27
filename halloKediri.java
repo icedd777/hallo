83b83aea7dd15a04633c9b43835eabcac4029629
icedd777 <dumatubunrisat@gmail.com>
2024-12-27 17:01:01 +0700

    Add files via upload

---
+++ halloKediri.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hallo;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class halloKediri {
+    public static void main(String[] args) {
+        System.out.println("HALLO JATIM");
+        System.out.println("selamat malam warga jatim");
+        System.out.println("selamat tahun baru warga jatim");
+    }
+    
+}
---
+++ hallojepang.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hallo;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class hallojepang {
+    public static void main(String[] args) {
+        System.out.println("hallo kawan");
+    }
+}
